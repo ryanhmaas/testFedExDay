84bc77cda23f066e1d72340ce8a6d3b34e80ce7c
Ryan Maas <ryan.h.maas@gmail.com>
2015-11-19 16:07:57 -0500

    Some styling.

--- public/js/nfl_scraper.js
+++ public/js/nfl_scraper.js
@@ -1,12 +1,13 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-/*
+
 request('http://sports.yahoo.com/nfl/', function(error, response, html){
     if(!error && response.statusCode == 200){
       var $ = cheerio.load(html);
       $('ol li div.game').each(function(i, element){
         var time = $(element).find('.state').text();
+        console.log(time);
         time = time.replace(/(\r\n|\n|\r)/gm,"");
         var teams = $(element).find('.team').text();
         teams = teams.replace(/(\r\n|\n|\r)/gm,"");
@@ -15,17 +16,6 @@ request('http://sports.yahoo.com/nfl/', function(error, response, html){
           teams: teams
         }
        console.log(data);
-      });
-    }
-});
-*/
-
-
-request('http://nfl.com', function(error, response, html){
-    if(!error && response.statusCode == 200){
-      var $ = cheerio.load(html);
-      $('body div div div div div ol li div a.yui3-scorestrip-tile-game-link').each(function(i, element){
-        console.log($(this).html());
       });
     }
 });
