4721dbe8d499c2adbb15dfb89305b41980f7d1f1
Ryan Maas <ryan.h.maas@gmail.com>
2015-11-20 13:32:13 -0500

    Fixed JSON routing, added grayscale hover effects

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -5,7 +5,7 @@ angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', f
 $routeProvider
     // home page
     .when('/', {
-        templateUrl: 'views/home.html',
+        templateUrl: 'views/index.html',
         controller: 'MainController'
     });
 
