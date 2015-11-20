24817f69622613d08c76ed56bb2aa5591d73aed2
Ryan Maas <ryan.h.maas@gmail.com>
2015-11-20 00:13:21 -0500

    Task runner to run scraper every minute...will change as needed.

--- app/tasks/nfl_scraper.js
+++ app/tasks/nfl_scraper.js
@@ -1,60 +1,62 @@
-var request = require('request');
-var cheerio = require('cheerio');
-var fs      = require('fs');
+module.exports = {
+  runScraper: function(){
+    var request        = require('request');
+    var cheerio        = require('cheerio');
+    var fs             = require('fs');
+    request('http://sports.yahoo.com/nfl/', function(error, response, html){
+        if(!error && response.statusCode == 200){
+          var $ = cheerio.load(html);
+          var jsonArr = [];
+          $('ol li div.game').each(function(i, element){
+
+            //live or final
+            var state = $(element).find('.state').text();
+            state = state.replace(/(\r\n|\n|\r)/gm,"");
+
+            //quarter and time
+            var status = $(element).find('.period').text();
+
+            //scores
+            var homeScore     = $(element).find('.team .score em').first().text();
+            var visitingScore = $(element).find('.team .score em').last().text();
+
+            //teams
+           var homeTeam = $(element).find('.team').first()
+            .clone()    //clone the element
+            .children() //select all the children
+            .remove()   //remove all the children
+            .end()  //again go back to selected element
+            .text();
 
 
-request('http://sports.yahoo.com/nfl/', function(error, response, html){
-    if(!error && response.statusCode == 200){
-      var $ = cheerio.load(html);
-      var jsonArr = [];
-      $('ol li div.game').each(function(i, element){
-
-        //live or final
-        var state = $(element).find('.state').text();
-        state = state.replace(/(\r\n|\n|\r)/gm,"");
-
-        //quarter and time
-        var status = $(element).find('.period').text();
-
-        //scores
-        var homeScore     = $(element).find('.team .score em').first().text();
-        var visitingScore = $(element).find('.team .score em').last().text();
+            var visitingTeam = $(element).find('.team').last()
+            .clone()    //clone the element
+            .children() //select all the children
+            .remove()   //remove all the children
+            .end()  //again go back to selected element
+            .text();
 
 
-        //teams
-       var homeTeam = $(element).find('.team').first()
-        .clone()    //clone the element
-        .children() //select all the children
-        .remove()   //remove all the children
-        .end()  //again go back to selected element
-        .text();
+            visitingTeam = visitingTeam.replace(/(\r\n|\n|\r)/gm,"");
+            homeTeam = homeTeam.replace(/(\r\n|\n|\r)/gm,"");
+            visitingTeam = visitingTeam.replace(/\s+/g, '');
+            homeTeam = homeTeam.replace(/\s+/g, '');
 
-
-        var visitingTeam = $(element).find('.team').last()
-        .clone()    //clone the element
-        .children() //select all the children
-        .remove()   //remove all the children
-        .end()  //again go back to selected element
-        .text();
-
-
-        visitingTeam = visitingTeam.replace(/(\r\n|\n|\r)/gm,"");
-        homeTeam = homeTeam.replace(/(\r\n|\n|\r)/gm,"");
-        visitingTeam = visitingTeam.replace(/\s+/g, '');
-        homeTeam = homeTeam.replace(/\s+/g, '');
-
-        var gameData = {
-          state: state,
-          status: status,
-          homeTeam: homeTeam,
-          homeScore: homeScore,
-          visitingTeam: visitingTeam,
-          visitingScore: visitingScore
+            var gameData = {
+              state: state,
+              status: status,
+              homeTeam: homeTeam,
+              homeScore: homeScore,
+              visitingTeam: visitingTeam,
+              visitingScore: visitingScore
+            }
+            jsonArr.push(gameData);
+          });
+          fs.writeFile('output.json', JSON.stringify(jsonArr, null, 4), function(err){
+            console.log('File successfully written!!!!! - Check your project directory for the output.json file');
+          });
         }
-        jsonArr.push(gameData);
-      });
-      fs.writeFile('output.json', JSON.stringify(jsonArr, null, 4), function(err){
-        console.log('File successfully written! - Check your project directory for the output.json file');
-      });
-    }
-});
+    });
+
+  }
+}
