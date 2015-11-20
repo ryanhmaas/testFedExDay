c48ee14234a80a7b5e976d173664dbf03900f3bb
Alex Valis <asvalis2@gmail.com>
2015-11-20 02:02:20 -0500

    Edited to take the output JSON to display on the page.
    It does not currently work but it is a starting point because I am tired
    as fuck. Also there is a bug in the scraper that does not record the
    score if the state is final. I think.

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,14 @@
 //Client-Side app.js
 //fix these as needed
 angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl']);
+
+
+
+var scrapeData = angular.module('App', []);
+
+scrapeData.controller('TodoCtrl', function($scope, $http) {
+  $http.get('output.json')
+       .then(function(res){
+          $scope.todos = res.data;                
+        });
+});
