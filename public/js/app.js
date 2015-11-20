5aa2ab03772a546bc2309ebcf3571af5cbe284ef
Alex Valis <asvalis2@gmail.com>
2015-11-20 09:23:05 -0500

    Fixes to the MainCtrl
    Tital

--- public/js/app.js
+++ public/js/app.js
@@ -3,12 +3,3 @@
 angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl']);
 
 
-
-var scrapeData = angular.module('scrapeData', []);
-
-scrapeData.controller('TodoCtrl', function($scope, $http) {
-  $http.get('output.json')
-       .then(function(res){
-          $scope.todos = res.data;                
-        });
-});
--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,6 +1,12 @@
 //test
 // public/js/controllers/MainCtrl.js
-angular.module('sampleApp', []).controller('MainCtrl', function($scope) {
+angular.module('sampleApp', []).controller('MainCtrl', function($scope, $http) {
+
+var scrapeData = angular.module('scrapeData', []);
+  $http.get('output.json')
+       .then(function(res){
+          $scope.output = res.data;                
+        });
 
     $scope.tagline = 'To the moon and back!';
 });
