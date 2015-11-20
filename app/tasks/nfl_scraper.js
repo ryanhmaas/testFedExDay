c416417aec788b4ec66a0c2c4f5529a65d85b22a
Ryan Maas <ryan.h.maas@gmail.com>
2015-11-19 21:49:50 -0500

    Scraper finalized

--- app/tasks/nfl_scraper.js
+++ app/tasks/nfl_scraper.js
@@ -1,26 +1,60 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var fs      = require('fs');
-var CronJob = require('cron').CronJob;
-var job = new CronJob('0 */15 * * * *', function(){
-  request('http://sports.yahoo.com/nfl/', function(error, response, html){
-      if(!error && response.statusCode == 200){
-        var $ = cheerio.load(html);
-        var json = {time: "", teams:""};
-        $('ol li div.game').each(function(i, element){
-          var time = $(element).find('.state').text();
-          time = time.replace(/(\r\n|\n|\r)/gm,"");
 
-          var teams = $(element).find('.team').text();
-          teams = teams.replace(/(\r\n|\n|\r)/gm,"");
 
-          json.time = time;
-          json.teams = teams;
-        });
-      }
-  });
-  fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
-      console.log('File successfully written! - Check your project directory for the output.json file');
-  });
+request('http://sports.yahoo.com/nfl/', function(error, response, html){
+    if(!error && response.statusCode == 200){
+      var $ = cheerio.load(html);
+      var jsonArr = [];
+      $('ol li div.game').each(function(i, element){
+
+        //live or final
+        var state = $(element).find('.state').text();
+        state = state.replace(/(\r\n|\n|\r)/gm,"");
+
+        //quarter and time
+        var status = $(element).find('.period').text();
+
+        //scores
+        var homeScore     = $(element).find('.team .score em').first().text();
+        var visitingScore = $(element).find('.team .score em').last().text();
+
+
+        //teams
+       var homeTeam = $(element).find('.team').first()
+        .clone()    //clone the element
+        .children() //select all the children
+        .remove()   //remove all the children
+        .end()  //again go back to selected element
+        .text();
+
+
+        var visitingTeam = $(element).find('.team').last()
+        .clone()    //clone the element
+        .children() //select all the children
+        .remove()   //remove all the children
+        .end()  //again go back to selected element
+        .text();
+
+
+        visitingTeam = visitingTeam.replace(/(\r\n|\n|\r)/gm,"");
+        homeTeam = homeTeam.replace(/(\r\n|\n|\r)/gm,"");
+        visitingTeam = visitingTeam.replace(/\s+/g, '');
+        homeTeam = homeTeam.replace(/\s+/g, '');
+
+        var gameData = {
+          state: state,
+          status: status,
+          homeTeam: homeTeam,
+          homeScore: homeScore,
+          visitingTeam: visitingTeam,
+          visitingScore: visitingScore
+        }
+        jsonArr.push(gameData);
+      });
+      fs.writeFile('output.json', JSON.stringify(jsonArr, null, 4), function(err){
+        console.log('File successfully written! - Check your project directory for the output.json file');
+      });
+    }
 });
-job.start();
